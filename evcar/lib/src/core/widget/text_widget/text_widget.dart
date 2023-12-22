38a42cf8a9d0b8721a35eb4beb0e62b3a28c4e27
ali7arb <138344082+ali7arb@users.noreply.github.com>
2023-12-23 02:38:59 +0300

    add textStyle

--- evcar/lib/src/core/widget/text_widget/text_widget.dart
+++ evcar/lib/src/core/widget/text_widget/text_widget.dart
@@ -1,6 +1,59 @@
 import 'package:flutter/material.dart';
 
 abstract class Styles {
-  static const textStyle18 =
-      TextStyle(fontSize: 18, fontFamily: 'cairo-Medium');
+  static const textStyle35 = TextStyle(
+    fontSize: 35,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'Cairo-Regular',
+  );
+  static const textStyle32 = TextStyle(
+    fontSize: 32,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'Cairo-Regular',
+  );
+  static const textStyle23 = TextStyle(
+    fontSize: 23,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'Cairo-Regular',
+  );
+  static const textStyle22 = TextStyle(
+    fontSize: 22,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'Cairo-Regular',
+  );
+  static const textStyle19 = TextStyle(
+    fontSize: 19,
+    fontWeight: FontWeight.w500,
+    fontFamily: 'Cairo-Regular',
+  );
+  static const textStyle18 = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Cairo-Regular',
+  );
+  static const textStyle17 = TextStyle(
+    fontSize: 17,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'Cairo-Regular',
+  );
+  static const textStyle14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Cairo-Regular',
+  );
+  static const textStyle12 = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Cairo-Regular',
+  );
+  static const textStyle10 = TextStyle(
+    fontSize: 10,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Cairo-Regular',
+  );
+  static const textStyle9 = TextStyle(
+    fontSize: 9,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Cairo-Regular',
+  );
 }
