19820a33909e9e09dd764e116cd02020e68070bd
unknown <alhamedabdullah@outlook.com>
2024-02-13 23:31:27 +0300

    finshing select and edit vendor

--- evcar/lib/src/feature/vendor_dashboard/view/widget/main_widget/vendor_dashboard_widget.dart
+++ evcar/lib/src/feature/vendor_dashboard/view/widget/main_widget/vendor_dashboard_widget.dart
@@ -1,17 +1,35 @@
 import 'package:evcar/src/config/sizes/sizes.dart';
 import 'package:evcar/src/feature/ads/view/pages/ads_page.dart';
 import 'package:evcar/src/feature/basic_info/view/page/basic_info_page.dart';
+import 'package:evcar/src/feature/login/controller/login_controller.dart';
+import 'package:evcar/src/feature/register/controller/user_register_controller.dart';
 import 'package:evcar/src/feature/services/view/page/vendor_services.dart';
 import 'package:evcar/src/feature/vendor_all_product/view/page/vendor_all_product_page.dart';
 import 'package:evcar/src/feature/vendor_dashboard/model/category_model.dart';
 import 'package:evcar/src/feature/vendor_dashboard/view/widget/widget_collection/dashboard_category.dart';
 import 'package:evcar/src/feature/vendor_dashboard/view/widget/widget_collection/vendor_appbar.dart';
+import 'package:evcar/test_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
 
-class VendorDashboardWidget extends StatelessWidget {
+class VendorDashboardWidget extends StatefulWidget {
   const VendorDashboardWidget({super.key});
+
+  @override
+  State<VendorDashboardWidget> createState() => _VendorDashboardWidgetState();
+}
+
+class _VendorDashboardWidgetState extends State<VendorDashboardWidget> {
+  final registerToken = Get.put(UserRegisterController());
+  final loginToken = Get.put(LoginController());
+  final selectServiceController = Get.put(SelectServiceController());
+  @override
+  void initState() {
+    selectServiceController.getVendorServiceDetails(
+        registerToken.token.value + loginToken.token.value);
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
